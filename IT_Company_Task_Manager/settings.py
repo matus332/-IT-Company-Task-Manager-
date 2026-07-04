ff9923ca72fa4641b746f4795b7ffe109caf0a9c
Denys Matusevych <denytop3@gmal.com>
2026-07-04 14:41:25 +0300

    Configure custom worker model

--- IT_Company_Task_Manager/settings.py
+++ IT_Company_Task_Manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "catalog.Worker"
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
